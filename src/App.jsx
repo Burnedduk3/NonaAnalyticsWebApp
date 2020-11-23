4e414363fb3532a4d633e7fbec0b81f16a8a7ba0
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2020-11-23 17:29:43 -0500

    fixed jslint command

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Amplify from 'aws-amplify';
 import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
-import awsExports from './aws-exports';
 import RouterConfig from './navigation/RouterConfig';
-
-Amplify.configure(awsExports);
 
 const App = () => (
   <BrowserRouter>
