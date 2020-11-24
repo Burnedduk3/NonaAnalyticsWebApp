69b7daab308113e3996d41e071fdd9dd96ef9c9a
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2020-11-23 22:03:59 -0500

    amplify configure

--- src/Pages/Form/InitialQuestions/index.jsx
+++ src/Pages/Form/InitialQuestions/index.jsx
@@ -1,9 +1,8 @@
 import React, { useContext, useEffect } from 'react';
-import { graphqlOperation, API } from 'aws-amplify';
+// import { graphqlOperation, API } from 'aws-amplify';
 import InitialQuestionHeader from '../../../Components/Form/InitialQuestionsComponents/Header';
 import DropConsole from '../../../utils/DropConsole';
-import { HIGH, LOW, MESSAGE } from '../../../utils/DropConsole/CONSTANTS';
-import { listQuestions as ListQuestions } from '../../../graphql/queries';
+import { HIGH, LOW } from '../../../utils/DropConsole/CONSTANTS';
 import { FormQuestionsContext } from '../../../Context/FormQuestions/Provider';
 
 const InitialFormPage = () => {
@@ -11,8 +10,8 @@ const InitialFormPage = () => {
 
   const getQuestions = async () => {
     try {
-      const apiQuestions = await API.graphql(graphqlOperation(ListQuestions));
-      DropConsole(MESSAGE, apiQuestions);
+      // const apiQuestions = await API.graphql(graphqlOperation(ListQuestions));
+      // DropConsole(MESSAGE, apiQuestions);
       DropConsole(LOW, state);
     } catch (e) {
       if (e instanceof Error) {
