7e5bed16073d58b87fbab06e7aeef8fe29d45076
Andres Felipe Gonzalez <andresfelipegonzalez@hotmail.com>
2020-11-24 18:35:19 -0500

    Basic Header container for home, added images from drive

--- src/Components/Header/PublicHeader/index.jsx
+++ src/Components/Header/PublicHeader/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './styles.scss';
-import HeaderImage from '../../../assets/Images/HomeImage.jpg';
 
 const PublicHeader = () => (
   <header className="header">
-    <img src={HeaderImage} alt="welcome" className="welcome-image" />
-    <h1 className="main-title">LAKE NONA LIFE PROJECT</h1>
-    <h2 className="sub-title">NONA ANALYTICS LLC</h2>
+    <div className="header-container" />
+    <h1 className="main-title">We want to build a community that promotes well-being</h1>
+    <button type="button" className="call-to-action">Learn more</button>
   </header>
 );
 
--- src/Pages/Public/Home/index.jsx
+++ src/Pages/Public/Home/index.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import './styles.scss';
 import PublicHeader from '../../../Components/Header/PublicHeader';
 
 const Home = () => (
-  <PublicHeader />
+  <>
+    <PublicHeader />
+    <div className="our-mission">
+      <div className="our-mission-content">
+        <h3 className="subtitle">Our Mission</h3>
+        <p>
+          Co-innovate with lifestyle medicine providers, fitness and wellness
+          centers, sports performance clubs, and health research institutions.
+          Our company utilizes state-of-the-art people analytics and machine
+          learning based personalization to offer innovative solutions that are
+          designed to double the analytical productivity of our customers, in
+          half of the time and with half the cost. Our team is located in Lake
+          Nona, the global epicenter for innovation in health, wellness and
+          sports technology. We are inspired to positively impact individual’s
+          health locally and internationally.
+        </p>
+      </div>
+    </div>
+    <div className="what-do-we-do">
+      <h3 className="subtitle">What do we do</h3>
+    </div>
+    <div className="our-team">
+      <h3 className="subtitle">Our Team</h3>
+    </div>
+  </>
 );
 
 export default Home;
