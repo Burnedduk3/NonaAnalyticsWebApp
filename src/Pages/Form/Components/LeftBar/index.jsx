19605f32400aeed555f79bb3e2fd89f9942cd568
estebanmz1342 <juanestebanmunoz.jems@gmail.com>
2020-11-29 14:26:57 -0500

    side bar complete

--- src/Pages/Form/Components/LeftBar/index.jsx
+++ src/Pages/Form/Components/LeftBar/index.jsx
@@ -3,13 +3,18 @@ import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import './styles.scss';
 import colors from '../../../../Global/js/colors';
+import logo from '../../../../assets/Logos/logo.png';
 import { UserContext } from '../../../../Context/UserContext/Provider';
 import DropDownComponent from './DropDown';
+import LeftBarText from './CONSTANTS';
 
 const LeftBar = () => {
   const { UserState } = useContext(UserContext);
   return (
     <aside className="aside-menu">
+      <div className="form-nona-logo-container">
+        <img className="form-nona-logo" src={logo} alt="Nonalogo" />
+      </div>
       <div className="progress-bar-container">
         <CircularProgressbar
           value={UserState.respondedQuestions}
@@ -20,8 +25,17 @@ const LeftBar = () => {
             textColor: colors.creamWhiteBackground,
           })}
         />
+        <p className="progress-bar-subtitle">Survey Progress</p>
       </div>
       <DropDownComponent />
+      <div className="last-section">
+        <h4 className="last-section-title">{LeftBarText.lastSection.title}</h4>
+        <p className="last-section-content">{LeftBarText.lastSection.content}</p>
+        <div className="social">
+          <i className="fab fa-instagram" />
+          <i className="fab fa-facebook-f" />
+        </div>
+      </div>
       <menu />
     </aside>
   );
