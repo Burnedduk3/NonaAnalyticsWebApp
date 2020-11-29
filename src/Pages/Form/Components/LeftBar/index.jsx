ec0e8308d5e3ff276df5ecbd54aba2b8d97d7b28
estebanmz1342 <juanestebanmunoz.jems@gmail.com>
2020-11-29 13:18:13 -0500

    added DropDownComponent

--- src/Pages/Form/Components/LeftBar/index.jsx
+++ src/Pages/Form/Components/LeftBar/index.jsx
@@ -4,6 +4,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import './styles.scss';
 import colors from '../../../../Global/js/colors';
 import { UserContext } from '../../../../Context/UserContext/Provider';
+import DropDownComponent from './DropDown';
 
 const LeftBar = () => {
   const { UserState } = useContext(UserContext);
@@ -20,43 +21,7 @@ const LeftBar = () => {
           })}
         />
       </div>
-      <div className="drop-down-menu">
-        <ul>
-          <li className="main-options" id="lake">Lake Nona</li>
-          <li className="main-options" id="health">
-            <a href="#health" className="btn">Health</a>
-            <div className="submenu" id="health-submenu">
-              <a href="#health-submenu" className="options">Demographics</a>
-              <a href="#health-submenu" className="options">Quality of Life</a>
-              <a href="#health-submenu" className="options">Your Health (Doctor, Prescription)</a>
-              <a href="#health-submenu" className="options">Health Behaviors</a>
-            </div>
-          </li>
-          <li className="main-options" id="mood">
-            <a href="#mood" className="btn">Mood</a>
-            <ul className="submenu" id="mood-submenu">
-              <a href="#mood-submenu" className="options">Oral Health</a>
-              <a href="#mood-submenu" className="options">Health Behaviors (Nutrition, Exercise)</a>
-              <a href="#mood-submenu" className="options">Time Use</a>
-              <a href="#mood-submenu" className="options">Low Mood</a>
-              <a href="#mood-submenu" className="options">Pets</a>
-              <a href="#mood-submenu" className="options">High Mood</a>
-              <a href="#mood-submenu" className="options">Stressful Life Experiences</a>
-            </ul>
-          </li>
-          <li className="main-options" id="social">
-            <a href="#social" className="btn">Social</a>
-            <div className="submenu" id="social-submenu">
-              <a href="#social-submenu" className="options">Your Personality</a>
-              <a href="#social-submenu" className="options">Employment</a>
-              <a href="#social-submenu" className="options">Social Networks</a>
-              <a href="#social-submenu" className="options">Your Neighborhood</a>
-              <a href="#social-submenu" className="options">Religiousness & Spirituality</a>
-              <a href="#social-submenu" className="options">Sleep Quality Index</a>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <DropDownComponent />
       <menu />
     </aside>
   );
