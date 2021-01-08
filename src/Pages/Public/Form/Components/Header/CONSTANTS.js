e4cfb428551b81140bdf0721283c0142e2810deb
Nicobetancur <nicobetancur@ufl.edu>
2021-01-08 11:33:42 -0500

    fix auth

--- src/Pages/Public/Form/Components/Header/CONSTANTS.js
+++ src/Pages/Public/Form/Components/Header/CONSTANTS.js
@@ -1,5 +1,7 @@
 const TextConstants = {
-  mainParagraph: 'Your participation is essential to cracking-the-code to life-long health and well-being. Join the thousands of Lake Nona Citizens.',
+  mainParagraph: 'Your participation is essential to cracking-the-' +
+      'code to life-long health and well-being.' +
+      ' Join the thousands of Lake Nona Citizens.',
   buttonText: 'Take Survey',
 };
 
