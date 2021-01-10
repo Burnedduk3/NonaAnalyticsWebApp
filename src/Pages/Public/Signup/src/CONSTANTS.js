0ce0151cf31728876a59ef1a5d9c753971a957e3
valdiba <mariadiba@unisabana.edu.co>
2021-01-10 14:27:33 -0500

    SignUp

---
+++ src/Pages/Public/Signup/src/CONSTANTS.js
@@ -0,0 +1,7 @@
+export default {
+  ButtonText: 'Sign Up',
+  AccountText: `Already have an account? `,
+  linkText: 'Login',
+  backButton: '   Back to Nona Analytics',
+};
+
--- src/Pages/Public/Signup/src/index.tsx
+++ src/Pages/Public/Signup/src/index.tsx
@@ -1,6 +1,10 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import {ISignUp} from '../interfaces/SignUpInterface';
 import {Auth} from 'aws-amplify';
+import {useHistory} from 'react-router-dom';
+import RoutingConstants
+  from '../../../../navigation/CONSTANTS/RoutingConstants';
 import {
   useApplicationState,
 } from '../../../../Context/ApplicationState/Provider';
@@ -8,6 +12,18 @@ import {
   HIDE_FOOTER,
   HIDE_HEADER,
 } from '../../../../Context/ApplicationState/ActionTypes';
+import './styles.scss';
+import CONSTANTS from './CONSTANTS.js';
+import logo from '../../../../assets/Logos/logo.png';
+import GoogleIcon from '../../../../assets/Icons/SocialMedia/google_color.png';
+import FBIcon from '../../../../assets/Icons/SocialMedia/facebook_color.png';
+import lock from '../../../../assets/Icons/lock.png';
+import user from '../../../../assets/Icons/user.png';
+import address from '../../../../assets/Icons/address.png';
+import birthday from '../../../../assets/Icons/birthday.png';
+import phone from '../../../../assets/Icons/phone.png';
+import mail from '../../../../assets/Icons/mail.png';
+import gender from '../../../../assets/Icons/gender.png';
 
 const initialInputState: ISignUp = {
   password: '',
@@ -23,11 +39,16 @@ const initialInputState: ISignUp = {
 const SignUpPage : React.FC = (): JSX.Element =>{
   const [pageInputs, setPageInputs] = useState(initialInputState);
   const applicationState = useApplicationState();
+  const history = useHistory();
 
   useEffect(()=>{
     applicationState?.appStateDispatch({type: HIDE_FOOTER, payload: undefined});
     applicationState?.appStateDispatch({type: HIDE_HEADER, payload: undefined});
   }, []);
+
+  const goHome = () =>{
+    history.push(RoutingConstants.menu.home.path);
+  };
 
   const handleInput = (event:ChangeEvent<HTMLInputElement>) => {
     if (event.target.name === 'address') {
@@ -88,40 +109,110 @@ const SignUpPage : React.FC = (): JSX.Element =>{
   };
 
   return (
-    <main>
-      <label htmlFor="address">
-          address
-        <input type="text" name='address' onChange={handleInput}/>
-      </label>
+    <main className="Sign-up-Body">
+      <div className="logoContainer">
+        <img src={logo} alt="Nona Logo"/>
+      </div>
+      <div className="Sign-up-Card">
+        <div className="go-back-container">
+          <p onClick={goHome}>
+            <i className="fas fa-caret-left"></i>
+            {CONSTANTS.backButton}
+          </p>
+        </div>
+        <h2>Sign Up</h2>
+        <div className="login-socialMedia">
+          <img src={GoogleIcon} alt="Google logo" />
+          <img src={FBIcon} alt="Facebook logo" />
+        </div>
+        <div className="horizontal-line">
+          <hr/>
+          O
+          <hr/>
+        </div>
+        <div className="form-container">
+          <label htmlFor="address">
+            <img src={address} alt="Address Icons8"/>
+            <input type="text"
+              name='address'
+              placeholder="Address"
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="birthdate">birthdate
-        <input type="text" name='birthdate' onChange={handleInput}/>
-      </label>
+          <label htmlFor="birthdate">
+            <img src={birthday} alt="Birthday Icons8"/>
+            <input type="text"
+              name='birthdate'
+              placeholder='Birthday'
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="email">email
-        <input type="text" name='email' onChange={handleInput}/>
-      </label>
+          <label htmlFor="email">
+            <img src={mail} alt="Mail Icons8"/>
+            <input type="text"
+              name='email'
+              placeholder='Email'
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="gender">gender
-        <input type="text" name='gender' onChange={handleInput}/>
-      </label>
+          <label htmlFor="gender">
+            <img src={gender} alt="Gender Icons8"/>
+            <input type="text"
+              name='gender'
+              placeholder='Gender'
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="name">name
-        <input type="text" name='name' onChange={handleInput}/>
-      </label>
+          <label htmlFor="name">
+            <img src={user} alt="User Icons8"/>
+            <input type="text"
+              name='name'
+              placeholder='Name'
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="phoneNumber">phoneNumber
-        <input type="text" name='phoneNumber' onChange={handleInput}/>
-      </label>
+          <label htmlFor="phoneNumber">
+            <img src={phone} alt="Phone Icons8"/>
+            <input type="text"
+              name='phoneNumber'
+              placeholder='PhoneNumber'
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="password">password
-        <input type="password" name='password' onChange={handleInput}/>
-      </label>
+          <label htmlFor="password">
+            <img src={lock} alt="Lock Icons8"/>
+            <input type="password"
+              name='password'
+              placeholder='Password'
+              onChange={handleInput}
+            />
+          </label>
 
-      <label htmlFor="confirmPassword">confirmPassword
-        <input type="password" name='confirmPassword' onChange={handleInput}/>
-      </label>
-      <button type="button" onClick={startSignup}>Sign Up</button>
+          <label htmlFor="confirmPassword">
+            <img src={lock} alt="Lock Icons8"/>
+            <input type="password"
+              name='confirmPassword'
+              placeholder='Confirm Password'
+              onChange={handleInput}
+            />
+          </label>
+          <button type="button" onClick={startSignup}>
+            {CONSTANTS.ButtonText}
+          </button>
+        </div>
+        <div className="have-account-container">
+          <p className="have-account-text">
+            {CONSTANTS.AccountText}
+            <Link to={RoutingConstants.login.path}>{CONSTANTS.linkText}</Link>
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
