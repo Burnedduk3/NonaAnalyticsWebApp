9b267f0beb2d93874e074dfcade5a78a5710e638
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-01-19 10:19:37 -0500

    added save to aurora db

--- src/Pages/Private/Form/Questioner/SaveQuestionsToAurora.ts
+++ src/Pages/Private/Form/Questioner/SaveQuestionsToAurora.ts
@@ -9,43 +9,48 @@ interface tempObject {
 const saveQuestionsToAurora = async (
     questions: any,
 ) => {
-  const sendToDBObject: tempObject = {
-    bornInUSA: 0,
-    yearsInFl: 0,
-  };
+  try {
+    const sendToDBObject: tempObject = {
+      bornInUSA: 0,
+      yearsInFl: 0,
+    };
 
-  const keyArray = Object.keys(questions).map((item) => item);
+    const keyArray = Object.keys(questions).map((item) => item);
 
-  for (let i = 0; i < keyArray.length; i++) {
-    if (i === 0) {
-      sendToDBObject.bornInUSA = parseInt(questions[keyArray[i]]);
+    for (let i = 0; i < keyArray.length; i++) {
+      if (i === 0) {
+        sendToDBObject.bornInUSA = parseInt(questions[keyArray[i]]);
+      }
+      if (i === 1) {
+        sendToDBObject.yearsInFl = questions[keyArray[i]] === 'yes' ? 1 : 0;
+      }
     }
-    if (i === 1) {
-      sendToDBObject.yearsInFl = questions[keyArray[i]] === 'yes'? 1: 0;
-    }
-  }
+    console.log(sendToDBObject);
 
-  const {bornInUSA, yearsInFl} = sendToDBObject;
+    const {bornInUSA, yearsInFl} = sendToDBObject;
 
-  if (bornInUSA && yearsInFl) {
-    const {data, errors}: any = await API.graphql(
-        graphqlOperation(
-            createDemographicInfo,
-            {
-              createDemographicInfoInput: {
-                id: Math.floor(Math.random() * 60) + 1,
-                bornInUSA: bornInUSA,
-                yearsInFl: yearsInFl,
+    if (bornInUSA && yearsInFl) {
+      const {data, errors}: any = await API.graphql(
+          graphqlOperation(
+              createDemographicInfo,
+              {
+                createDemographicInfoInput: {
+                  id: Math.floor(Math.random() * 60) + 1,
+                  bornInUSA: bornInUSA,
+                  yearsInFl: yearsInFl,
+                },
               },
-            },
-        ),
-    );
-
-    if (errors) {
-      throw new Error('Error sending responses to de Database');
+          ),
+      );
+      console.log(data);
+      if (errors) {
+        throw new Error('Error sending responses to de Database');
+      }
+    } else {
+      throw new Error('No Current form ID or subsection to save info');
     }
-  } else {
-    throw new Error('No Current form ID or subsection to save info');
+  } catch (err) {
+    console.log(err);
   }
 };
 
