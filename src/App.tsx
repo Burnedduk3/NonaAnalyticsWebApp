6a9999f1f073346ee08e98bcbd2b7c4a1c962095
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-01-24 20:23:07 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.scss';
 import RouterConfig from './navigation/RouterConfig';
 import {BrowserRouter} from 'react-router-dom';
-import Amplify from 'aws-amplify';
+import Amplify, {API, Auth} from 'aws-amplify';
 import awsExports from './aws-exports';
 
 // eslint-disable-next-line
 Amplify.configure(awsExports);
+Amplify.register(Auth);
+Amplify.register(API);
+
 
 const App: React.FC = (): JSX.Element => (
   <BrowserRouter>
