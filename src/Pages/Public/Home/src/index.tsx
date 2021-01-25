33b1af42309b2193eb9261563b457646ab389fa4
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-01-25 17:58:45 -0500

    changes

--- src/Pages/Public/Home/src/index.tsx
+++ src/Pages/Public/Home/src/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import './styles.scss';
-import Perk from '../Components/Perk';
+// import Perk from '../Components/Perk';
 import HomeTexts from './CONSTANTS';
 import {
   useApplicationState,
@@ -26,23 +26,23 @@ const Home: React.FC = (): JSX.Element => {
           <p>{HomeTexts.sections.first.text}</p>
         </div>
       </div>
-      <div className="what-do-we-do">
-        <h3 className="subtitle">{HomeTexts.title}</h3>
-        <div className="perks">
-          {
-            Object.values(HomeTexts.sections.second).map(
-                (item) => (
-                  <Perk
-                    key={item.title}
-                    img={item.img}
-                    title={item.title}
-                    text={item.text}
-                  />
-                ),
-            )
-          }
-        </div>
-      </div>
+      {/* <div className="what-do-we-do">*/}
+      {/*  <h3 className="subtitle">{HomeTexts.title}</h3>*/}
+      {/*  <div className="perks">*/}
+      {/*    {*/}
+      {/*      Object.values(HomeTexts.sections.second).map(*/}
+      {/*          (item) => (*/}
+      {/*            <Perk*/}
+      {/*              key={item.title}*/}
+      {/*              img={item.img}*/}
+      {/*              title={item.title}*/}
+      {/*              text={item.text}*/}
+      {/*            />*/}
+      {/*          ),*/}
+      {/*      )*/}
+      {/*    }*/}
+      {/*  </div>*/}
+      {/* </div>*/}
       <div className="our-team">
       </div>
     </>
