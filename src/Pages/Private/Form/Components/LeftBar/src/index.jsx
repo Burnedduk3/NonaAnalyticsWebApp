01e0bcec14adfbdf4e37b71401d75686d8296965
Burnedduk3 <juandavid.juandis@gmail.com>
2021-01-25 13:09:34 -0500

    added font and fixed side bar

--- src/Pages/Private/Form/Components/LeftBar/src/index.jsx
+++ src/Pages/Private/Form/Components/LeftBar/src/index.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import {buildStyles, CircularProgressbar} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import './styles.scss';
 import colors from '../../../../../../Global/js/colors';
 import logo from '../../../../../../assets/Logos/logo.png';
 import DropDownComponent from '../DropDown';
 import LeftBarText from './CONSTANTS';
+import './styles.scss';
+
 
 const LeftBar = () => {
   return (
     <aside className="aside-menu">
-      <div className="form-nona-logo-container">
-        <img className="form-nona-logo" src={logo} alt="Nonalogo" />
-      </div>
-      <div className="progress-bar-container">
-        <CircularProgressbar
-          value={50}
-          text={`${50}%`}
-          styles={buildStyles({
-            trailColor: colors.progressBackGround,
-            pathColor: colors.secondaryColor,
-            textColor: colors.creamWhiteBackground,
-          })}
-        />
-        <p className="progress-bar-subtitle">Survey Progress</p>
+      <div className="header-aside-container">
+        <div className="form-nona-logo-container">
+          <img className="form-nona-logo" src={logo} alt="Nonalogo" />
+        </div>
+        <div className="progress-bar-container">
+          <CircularProgressbar
+            value={50}
+            text={`${50}%`}
+            styles={buildStyles({
+              trailColor: colors.progressBackGround,
+              pathColor: colors.secondaryColor,
+              textColor: colors.creamWhiteBackground,
+            })}
+          />
+          <p className="progress-bar-subtitle">Survey Progress</p>
+        </div>
       </div>
       <DropDownComponent />
       <div className="last-section">
@@ -35,7 +38,6 @@ const LeftBar = () => {
           <i className="fab fa-facebook-f" />
         </div>
       </div>
-      <menu />
     </aside>
   );
 };
