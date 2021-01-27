a51af3c51dd063e4c98802226559b9aba636be48
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-01-27 11:44:58 -0500

    added menu functionality

--- src/Pages/Private/Form/Components/LeftBar/DropDown/index.jsx
+++ src/Pages/Private/Form/Components/LeftBar/DropDown/index.jsx
@@ -6,48 +6,126 @@ const DropDownComponent = () => (
   <div className="drop-down-menu">
     <ul>
       <li className="main-options" id="lake">
-        <a href="#lake" className="btn">Lake Nona</a>
+        <Link
+          to="/questioner/Lake-Nona/Lake-Nona/0"
+          className="btn"
+        >
+          Lake Nona
+        </Link>
         <span className="menu-status">1</span>
       </li>
       <li className="main-options" id="health">
-        <a href="#health" className="btn">Health</a>
+        <a className="btn">Health</a >
         <span className="menu-status">38</span>
         <div className="submenu" id="health-submenu">
-          <Link to="#health-submenu" className="options">Demographics</Link>
-          <Link to="#health-submenu" className="options">Quality of Life</Link>
+          <Link
+            to="/questioner/Health/Demographics/0"
+            className="options"
+          >
+            Demographics
+          </Link>
+          <Link
+            to="/questioner/Health/Quality-of-Life/0"
+            className="options"
+          >
+            Quality of Life
+          </Link>
           {/* eslint-disable-next-line max-len */}
-          <Link to="#health-submenu" className="options">Your Health (Doctor, Prescription)</Link>
-          <Link to="#health-submenu" className="options">Health Behaviors</Link>
+          <Link
+            to="/questioner/Health/Your-Health/0"
+            className="options"
+          >
+            Your Health (Doctor, Prescription)
+          </Link>
+          <Link
+            to="/questioner/Health/Health-Behaviors/0"
+            className="options"
+          >
+            Health Behaviors
+          </Link>
         </div>
       </li>
       <li className="main-options" id="mood">
-        <a href="#mood" className="btn">Mood</a>
+        <a className="btn">Mood</a>
         <span className="menu-status">28</span>
         <ul className="submenu" id="mood-submenu">
-          <Link to="#mood-submenu" className="options">Oral Health</Link>
-          {/* eslint-disable-next-line max-len */}
-          <Link to="#mood-submenu" className="options">Health Behaviors (Nutrition, Exercise)</Link>
-          <Link to="#mood-submenu" className="options">Time Use</Link>
-          <Link to="#mood-submenu" className="options">Low Mood</Link>
-          <Link to="#mood-submenu" className="options">Pets</Link>
-          <Link to="#mood-submenu" className="options">High Mood</Link>
-          {/* eslint-disable-next-line max-len */}
-          <Link to="#mood-submenu" className="options">Stressful Life Experiences</Link>
+          <Link
+            to="/questioner/Mood/Oral-Healt/0"
+            className="options"
+          >
+            Oral Health
+          </Link>
+          <Link
+            to="/questioner/Mood/Health-Behavior/0"
+            className="options"
+          >
+            Health Behaviors (Nutrition, Exercise)
+          </Link>
+          <Link
+            to="/questioner/Mood/Low-Mood/0"
+            className="options"
+          >
+            Low Mood
+          </Link>
+          <Link
+            to="/questioner/Mood/Pets/0"
+            className="options"
+          >
+            Pets
+          </Link>
+          <Link
+            to="/questioner/Mood/High-Mood/0"
+            className="options">
+            High Mood
+          </Link>
+          <Link
+            to="/questioner/Mood/Stressful-Life-Experience/0"
+            className="options"
+          >
+            Stressful Life Experiences
+          </Link>
         </ul>
       </li>
       <li className="main-options" id="social">
-        <a href="#social" className="btn">Social</a>
+        <a className="btn">Social</a>
         <span className="menu-status">22</span>
         <ul className="submenu" id="social-submenu">
-          <Link to="#social-submenu" className="options">Your Personality</Link>
-          <Link to="#social-submenu" className="options">Employment</Link>
-          <Link to="#social-submenu" className="options">Social Networks</Link>
-          {/* eslint-disable-next-line max-len */}
-          <Link to="#social-submenu" className="options">Your Neighborhood</Link>
-          {/* eslint-disable-next-line max-len */}
-          <Link to="#social-submenu" className="options">Religiousness & Spirituality</Link>
-          {/* eslint-disable-next-line max-len */}
-          <Link to="#social-submenu" className="options">Sleep Quality Index</Link>
+          <Link
+            to="/questioner/Social/Your-Personality/0"
+            className="options"
+          >
+            Your Personality
+          </Link>
+          <Link
+            to="/questioner/Social/Employment/0"
+            className="options"
+          >
+            Employment
+          </Link>
+          <Link
+            to="/questioner/Social/Social-Network/0"
+            className="options"
+          >
+            Social Networks
+          </Link>
+          <Link
+            to="/questioner/Social/Your-Neighborhood/0"
+            className="options"
+          >
+            Your Neighborhood
+          </Link>
+          <Link
+            to="/questioner/Social/Religiousness-&-Spirituality/0"
+            className="options"
+          >
+            Religiousness & Spirituality
+          </Link>
+          <Link
+            to="/questioner/Social/Sleep-Quality/0"
+            className="options"
+          >
+            Sleep Quality Index
+          </Link>
         </ul>
       </li>
     </ul>
