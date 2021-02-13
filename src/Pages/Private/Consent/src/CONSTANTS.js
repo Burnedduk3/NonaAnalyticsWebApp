9b4af2cb6184d995250fd7392bc68730d96f0fcb
Juan David Cabrera Duran <juancabdu@unisabana.edu.co>
2021-02-13 12:27:00 -0500

    Added consent page

---
+++ src/Pages/Private/Consent/src/CONSTANTS.js
@@ -0,0 +1,155 @@
+/* tslint:disable */
+/* eslint-disable */
+export default {
+  header: {
+    mainTitle: 'Consent Terms',
+    subTitle: 'AdventHealth IRB Approved: 09/03/2020 IRBNet #: 1166275 May not be used for subject enrollment beyond: 08/05/2021 ',
+    titleOfResearchStudy: 'Title of research study: The Lake Nona Life Project',
+    investigator: 'Investigator: Patricia Robinson, PhD, ARNP',
+    address: '301 E Princeton Street ',
+    state: 'Orlando, FL 32804',
+    phone: 'Daytime Phone Number: 407-303-7128 ',
+    sponsor: 'Sponsor: AdventHealth ',
+  },
+  firstPart: {
+    title: 'Why am I being invited to take part in a research study?',
+    paragraph1: 'We invite you to take part in a research study because you are an adult or household member  age 18 and older, living, working, studying, and/or playing within the Lake Nona study zone  in Orlando, Florida.',
+    paragraph2: 'If you have previously consented and are currently enrolled in this study, you are simply  being asked to read this updated document and re-consent at this time.',
+  },
+  secondPart: {
+    tilte: 'What should I know about a research study? ',
+    listElements: {
+      listElement1: 'Whether or not you take part is up to you.',
+      listElement2: 'You can choose not to take part.',
+      listElement3: 'You can agree to take part and later change your mind.',
+      listElement4: 'Your decision will not be held against you. ',
+      listElement5: 'You can ask all the questions you want before you decide. ',
+      listElement6: ' If you are an employee of AdventHealth or Tavistock Group, you should know that your  participation or lack of participation in this study will not affect your employment or  relationship with your employer. ',
+    },
+  },
+  thirdPart: {
+    title: 'Why is this research being done?',
+    paragraph: 'The Lake Nona Life Project is a community health and wellness research study of the Lake  Nona Institute (LNI), carried out by the Lake Nona Medical City Residents and Partners (the  Researchers and the Sponsors). The research study is designed to study patterns, predictors,  and strategies for improving human health and wellness. Data will be obtained over many  years using various methods such as surveys, interviews, medical tests, and health  information from 3rd parties. Data will be analyzed on a regular (yearly) basis. ',
+  },
+  FourthPart: {
+    title: 'How long will the research last?',
+    paragraph1: 'This study is longitudinal and does not have a set end date.',
+    paragraph2: 'Adults and household members age 18 and older living, working, studying, and/or playing  within Lake Nona in Orlando, Florida who can read and speak English are invited to  participate. ',
+    paragraph3: 'If you choose to participate, you will complete a confidential health survey as frequently as  every year that will ask about your background, attitudes, health (physical and mental), and  lifestyle. Surveys may take 30-40 minutes to complete. As a participant you may receive  optional additional questions, surveys, or sub-study invitations via email based upon your  survey responses. ',
+  },
+  FivethPart: {
+    title: 'What happens if I agree to be in this research?',
+    paragraph1: 'You have been provided with the website for the study. You will be asked to create an  account on the study’s survey platform and review this consent document. If you decide that  you would like to participate, and your questions have been answered regarding the study,  you will electronically sign this document. A copy of this document will be emailed to you. ',
+    paragraph2: 'If you choose to participate in the study, you will complete a confidential health survey as  frequently as every year that will ask about your background, attitudes, health (physical and  mental), and lifestyle. Future study components (e.g., sub-studies, surveys, or tests) are  expected and will require a separate consent. ',
+  },
+  sixthParth: {
+    title: 'What other choices do I have beside taking part in the research?',
+    paragraph1: 'Participation is entirely voluntary with no penalties or loss of benefits for stopping or  declining. You can choose how long to participate, if you want to answer only some survey  questions, or if you want to participate in only some or none of the related studies that may  be added later. ',
+    paragraph2: 'You can choose not to take part. You can agree to take part and later change your mind. If  you choose to no longer participate, you may contact the study team to withdraw or you may  withdraw yourself from the study’s survey platform. ',
+  },
+  seventhPart: {
+    title: 'Is there any way being in this study could be bad for me? ',
+    paragraph1: 'Risks of study participation (physical or non-physical) are minimal and are not expected to  be beyond those encountered in daily life. For instance, some questions may make you feel emotional or uncomfortable. You can stop your participation in this study at any time. ',
+  },
+  eightPart: {
+    title: 'Will being in this study help me in any way?',
+    paragraph: 'We cannot promise that the study will help you but participating in this study may make you  more aware of your health and wellness. Overall study results may help improve the health  and wellness of future generations. ',
+  },
+  ninthPart: {
+    title: 'What happens to the information collected for the research?',
+    paragraph1: 'We will protect your personal information. There are laws that help us keep your information  private. These laws say that some people can look at your information, for example IRB  members and some people who work for AdventHealth might look at it. They may copy it.  Only people who need to see your information may look at it. ',
+    paragraph2: 'All study data will be stored in a confidential data repository that meets the HIPAA security  standards and stored in accordance with applicable local, state, and federal laws. Individual  records will be assigned a confidential ID number and will be separated from your  personally identifiable information (e.g., name, address, phone number). The Researchers  will analyze only the encrypted data that does not identify you directly. The Sponsor may  share this encrypted data with third party partners for future research, so long as such  research partners legally agree to maintain the confidentiality of such information. The IRB  will be informed of any data sharing agreements. Study results presented at meetings and  published in scientific journals and books will only be reported in aggregate form without  identifying individual participants. A National Institutes of Health (NIH) Certificate of  Confidentiality (CoC) will further protect your confidential information from court subpoena  except where required by law for child abuse, intent to hurt yourself or others, or in Food and  Drug Administration (FDA) or other federal government audits. A CoC does not represent an  endorsement of the research study by the Department of Health and Human Services or the  NIH. ',
+    paragraph3: 'Upon audit, study staff, Sponsor and/or its representative(s), State and Federal regulatory  agencies (such as the FDA, monitors, and auditors), and the Institutional Review Board  (IRB) may also be given access. A description of this study will be available at http:// www.ClinicalTrials.gov as required by U.S. law. This is a public Web site but will not  include information that can identify you. At most, the Web site will include a summary of  the study results. You can search this Web site at any time. ',
+    paragraph4: 'Federal law provides additional protections of your health information. See the HIPAA  section below. ',
+  },
+  tenthPart: {
+    title: 'What else do I need to know?',
+    paragraph: 'This research is being funded by Lake Nona Institute, including AdventHealth. As a  participant in this research study, you will receive a thank you gift from the Lake Nona Life  Project Team upon completion of the survey. Once you have completed the survey, you will  receive a confirmation email with instructions to contact the Lake Nona Life Project Team at  lakenonalifeproject@lakenonainstitute.com to make arrangements to receive your gift. HIPAA Authorization to Release Information for Research',
+    paragraph2: 'If you have not received a copy of the AdventHealth Privacy Notice, please request one. If  you have questions about your privacy rights, you may contact AdventHealth’s Privacy  Officer at PH: (407) 303-9659. ',
+    paragraph3: 'Privacy laws, including the Health Insurance Portability & Accountability Act (HIPAA) and  other federal and state laws, rules, and regulations, protect your individually identifiable  health information (also called Protected Health Information or PHI). If you agree to be in  this study, privacy laws require you to sign this Authorization that describes your rights and  explains how your Protected Health Information (PHI) will be used and disclosed for this  research study. ',
+    paragraph4: 'By signing this informed consent/HIPAA Authorization, you will be authorizing the  principal investigator, his/her research staff, and the sponsor to use and disclose your PHI for  the purposes described below. ',
+    paragraph5: 'This Authorization does not have an expiration date. This means the researchers and others  associated with this study may use and disclose your protected health information for as long  as necessary to complete the study. ',
+    paragraph6: 'If you volunteer to take part in this research study, others may learn your identity. Study  information may identify you in the following ways. ',
+    listElements: {
+      element1: 'Name',
+      element2: 'Address',
+      element3: 'Telephone number',
+      element4: 'Other details about you',
+    },
+    paragraph7: 'This study includes researchers, businesses and government agencies. They may use your  health information and share it with others. We want you to know who may use this  information and how they may use it. ',
+  },
+  eleventhPart: {
+    title: 'Who may use and give out information about you? ',
+    paragraph1: 'The Investigator and research staff will have information about your health that tells us your  identity. They may share this information with others during and after the study. ',
+  },
+  twelfthPart: {
+    title: 'Who may see this information?',
+    paragraph1: 'The study sponsor may see your health information and know your identity. “Sponsor”  includes people or companies working for or with the sponsor or owned by the sponsor. ',
+    paragraph2: 'In addition to the study sponsor and its agents, the following people, agencies and businesses  may get information from us that identify who you are. ',
+    listElements: {
+      element1: 'Research partners and research teams taking part in the study;',
+      element2: 'U.S. Department of Health and Human Services (DHHS), which includes: · U.S. Food and Drug Administration (FDA)',
+      element3: 'U.S. Office of Human Research Protections (OHRP)',
+      element4: 'AdventHealth representatives',
+      element5: 'Institutional Review Board (IRB)',
+      element6: 'Accreditation organizations',
+    },
+  },
+  thirteenthPart: {
+    title: 'What information may be used and shared?',
+    paragraph: 'If you decide to be in this study, behavior and lifestyle information that identifies you and  relates to your participation will be created, used, and/or shared. This may include any  responses provided on the survey(s).',
+  },
+  FourteenthPart: {
+    title: 'Why will this information be used and/or shared?',
+    paragraph1: 'Information about you and your health, that might identify you, may be given to others to  carry out the research study. The sponsor and/or the investigator will analyze and evaluate  the results of the study. In addition, the Sponsor and its research staff will be visiting the  research site. They will follow how the study is done, and they will be reviewing your  information for this purpose. ',
+  },
+  fifteenthPart: {
+    title: 'What if I decide not to give permission to use and give out my health  information? ',
+    paragraph: 'If you sign this consent form, you will be giving permission to use and give out the health  information listed above for the purposes described above. If you decide not to give  permission, you will not be able to be in this research. However, this will not change your  relationship with AdventHealth, the Lake Nona Institute, and other study partners. You will  still be able to receive all benefits to which you are entitled. ',
+  },
+  sixteenthPart: {
+    title: 'May I review or copy the information obtained from me or created  about me? ',
+    paragraph: 'If you decide to be in this study and sign this permission form, you may be able to look at or  copy your research information at appropriate intervals. ',
+  },
+  seventeenth: {
+    title: 'May I withdraw or revoke (cancel) my permission? ',
+    paragraph: 'Yes. You must cancel your permission (withdraw from the study) by notifying a study team  member, by submitting cancellation in writing to the study team, or by withdrawing from the  study’s survey platform. If you do not cancel your permission, this permission form does not expire.',
+    paragraph2: 'If you withdraw your permission, you can change your mind and provide permission to  participate in the study at a later time. ',
+    paragraph3: 'If you withdraw your permission, we will not collect any more information about you.  Information that has already been gathered may still be used and given to others. ',
+    paragraph4: 'You may withdraw or take away your permission to use and disclose your health information  at any time. You may do this by sending written notice to the study investigator or  withdrawing yourself from the study’s survey platform. If you withdraw your permission,  you will not be able to continue being in this study. ',
+    paragraph5: 'If you want to withdraw your permission and not have your information shared beyond what  has already been shared, please send the written notice to: ',
+  },
+  eighteenth: {
+    subTitle1: 'The Lake Nona Life Project',
+    subTitle2: 'c/o AdventHealth Research Institute',
+    subTitle3: 'Attention: Patricia Robinson, PhD, ARNP',
+    subTitle4: '301 East Princeton Street',
+    subTitle5: 'Orlando, FL 32804',
+    subTitle6: 'Phone: 407-303-7128',
+    subTitle7: 'E-mail: Patricia.Robinson@AdventHealth.com ',
+  },
+  nineteenth: {
+    title: 'Is my health information protected after it has been given to others?',
+    paragraph1: 'When you sign this form, we may share your information with others. If we do, that  information might not be protected. There is a risk that your information will be released to  others without your permission. ',
+    paragraph2: 'Your personal information may be disclosed if required by law. Your records for this study  may be sent over the Internet. Your records could be sent to the wrong person. ',
+  },
+  twentieth: {
+    title: 'How long is my information kept?',
+    paragraph1: 'Research with private health information must be maintained for seven years after the  research study has been closed at the AdventHealth site. ',
+  },
+  twentyFirst: {
+    title: 'Who can I talk to?',
+    paragraph1: 'If you have questions, concerns, or complaints, or think the research has hurt you, talk to the  research team at 407-303-7128 or Patricia.Robinson@AdventHealth.com. This research is being overseen by an Institutional Review Board (“IRB”). The IRB is a  group of people who review and approve research at AdventHealth. You may talk to them at  (407) 200-2677 or FH.IRB.General@AdventHealth.com if: ',
+
+    listElements: {
+      element1: 'Your questions, concerns, or complaints are not being answered by the research team. ',
+      element2: 'You cannot reach the research team. ',
+      element3: 'You want to talk to someone besides the research team. ',
+      element4: 'You have questions about your rights as a research subject. ',
+      element5: 'You want to get information or provide input about this research.',
+    },
+    paragraph2: 'I have read and fully understand the terms of this agreement. ',
+  },
+};
+
+export const AcceptanceStatement = "I Accept everything on this consentment"
--- src/Pages/Private/Consent/src/index.tsx
+++ src/Pages/Private/Consent/src/index.tsx
@@ -1,21 +1,213 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles.scss';
-import {HIDE_FOOTER} from '../../../../Context/ApplicationState/ActionTypes';
+import {
+  HIDE_FOOTER, SET_ERROR,
+} from '../../../../Context/ApplicationState/ActionTypes';
 import {
   useApplicationState,
 } from '../../../../Context/ApplicationState/Provider';
+import CONSTANTS, {AcceptanceStatement} from './CONSTANTS';
+import './styles.scss';
+import {useFormQuestionState} from '../../../../Context/FormQuestions/Provider';
+import {ErrorMessageToast} from '../../../../Components/ErrorMessage';
+import {API, graphqlOperation} from 'aws-amplify';
+import {updateForm} from '../../../../graphql/mutations';
+import {UpdateFormMutationVariables} from '../../../../API';
+import {useHistory} from 'react-router-dom';
+import RoutingConstants
+  from '../../../../navigation/CONSTANTS/RoutingConstants';
 
 const Consent: React.FC = ():JSX.Element =>{
   const applicationState = useApplicationState();
+  const [acceptInvitation, setAcceptInvitation] = useState<boolean>(false);
+  const formState = useFormQuestionState();
+  const [toggleToast, setToggleToast] = useState<boolean>(false);
+  const [redirect, setRedirect] = useState<boolean>(false);
+  const history = useHistory();
+
+
   useEffect(()=>{
     applicationState.appStateDispatch({type: HIDE_FOOTER, payload: undefined});
   },
   []);
+
+  useEffect(()=>{
+    if (redirect) {
+      history.push(
+          RoutingConstants.dinamicForm.path,
+      );
+    }
+  },
+  [redirect]);
+
+
+  const onAcceptConsent = async () =>{
+    if (formState.formState.currentFormID && acceptInvitation) {
+      const {currentFormID} = formState.formState;
+      const formMutationVariables: UpdateFormMutationVariables = {
+        input: {
+          id: currentFormID,
+          consent: true,
+        },
+      };
+      try {
+        const response:any = await API.graphql(
+            graphqlOperation(
+                updateForm,
+                formMutationVariables,
+            ),
+        );
+        console.log(response);
+        if (response.data) {
+          setRedirect(true);
+        }
+      } catch (err) {
+        applicationState.appStateDispatch(
+            {
+              type: SET_ERROR,
+              payload: {
+                error: {
+                  error: true,
+                  errorMessage: err.message,
+                },
+              },
+            },
+        );
+      }
+    } else {
+      setToggleToast(true);
+    }
+  };
+
   return <>
     <main className="consent-container">
-      <div>
-        <h1>Consent page</h1>
+      {<ErrorMessageToast
+        message={
+          'If you dont accept you wont be able to participate'
+        }
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick={false}
+        pauseOnHover={false}
+        draggable={false}
+        toggleToast={toggleToast}
+        setToggleToast={setToggleToast}
+      />}
+      <div className="page-container">
+        <div className="header-container">
+          {
+            Object.entries(CONSTANTS.header).map(
+                (entry) =>{
+                  if (entry[0] === 'mainTitle') {
+                    return (
+                      <h1
+                        key={entry[0]}
+                        className={entry[0].toLowerCase()}
+                      >
+                        {entry[1]}
+                      </h1>
+                    );
+                  }
+                  if (entry[0] === 'subTitle') {
+                    return (
+                      <h2
+                        key={entry[0]}
+                        className={entry[0].toLowerCase()}
+                      >
+                        {entry[1]}
+                      </h2>
+                    );
+                  }
+                  return ( <h3
+                    key={entry[0]}
+                    className={entry[0].toLowerCase()}
+                  >
+                    {entry[1]}
+                  </h3>
+                  );
+                },
+            )
+          }
+        </div>
+        {
+          Object.entries(CONSTANTS).map(
+              (entry)=>{
+                if (entry[0] !== 'header') {
+                  return Object.entries(entry[1]).map(
+                      (htmlContent, index)=>{
+                        if (htmlContent[0] === 'title') {
+                          return (
+                            <h4
+                              className={htmlContent[0]}
+                              key={htmlContent[0] + index}
+                            >
+                              {htmlContent[1]}
+                            </h4>
+                          );
+                        }
+
+                        if (htmlContent[0].includes('paragraph')) {
+                          return (
+                            <p
+                              className="paragraph"
+                              key={htmlContent[0] + index}
+                            >
+                              {htmlContent[1]}
+                            </p>
+                          );
+                        }
+
+                        if (htmlContent[0] === 'listElements') {
+                          return (
+                            <ul
+                              className="list-container"
+                              key={htmlContent[0] + index}
+                            >
+                              {
+                                Object.values(htmlContent[1]).
+                                    map(
+                                        (listElement, index) =>{
+                                          return (
+                                            <li
+                                              className="list-element"
+                                              key={''+listElement}
+                                            >
+                                              {''+listElement}
+                                            </li>
+                                          );
+                                        },
+                                    )
+                              }
+                            </ul>
+                          );
+                        }
+                      },
+                  );
+                }
+              },
+          )
+        }
+        <div className={'input-container'}>
+          <label htmlFor="accept-invitation">
+            <input
+              name="accept-invitation"
+              type="checkbox"
+              checked={acceptInvitation}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>)=>{
+                setAcceptInvitation(event.target.checked);
+              }}
+            />
+            {AcceptanceStatement}
+          </label>
+        </div>
       </div>
+      <button
+        className="button"
+        onClick={()=> onAcceptConsent()}
+      >
+        Start Survey
+      </button>
     </main>
   </>;
 };
