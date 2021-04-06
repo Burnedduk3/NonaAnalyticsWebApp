efd414490c0926579d784339b3640c3c9ad0418e
Juan David Cabrera Duran <juancabdu@unisabana.edu.co>
2021-04-05 20:58:10 -0500

    added backend link

--- src/Config/EnviromentVariables/index.js
+++ src/Config/EnviromentVariables/index.js
@@ -1,4 +1,4 @@
 export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT ?
   process.env.REACT_APP_ENVIRONMENT : 'develop';
 
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:3500/graphql';
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'internal-prod-Back-end-balancer-463094779.us-east-1.elb.amazonaws.com:3500/graphql';
