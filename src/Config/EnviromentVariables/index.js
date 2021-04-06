f11e02c1b32274b1d8fe39f34663b65e02f6455b
Juan David Cabrera Duran <juancabdu@unisabana.edu.co>
2021-04-05 21:09:54 -0500

    fixed env variable:

--- src/Config/EnviromentVariables/index.js
+++ src/Config/EnviromentVariables/index.js
@@ -1,4 +1,5 @@
 export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT ?
   process.env.REACT_APP_ENVIRONMENT : 'develop';
 
+// eslint-disable-next-line max-len
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'internal-prod-Back-end-balancer-463094779.us-east-1.elb.amazonaws.com:3500/graphql';
