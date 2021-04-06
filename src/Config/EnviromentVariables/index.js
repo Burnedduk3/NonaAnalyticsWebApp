5d2be92b53ddd080d3a7e66f70f3a5f130481540
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-04-06 09:32:11 -0500

    added protocol to backend string

--- src/Config/EnviromentVariables/index.js
+++ src/Config/EnviromentVariables/index.js
@@ -2,4 +2,4 @@ export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT ?
   process.env.REACT_APP_ENVIRONMENT : 'develop';
 
 // eslint-disable-next-line max-len
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'internal-prod-Back-end-balancer-463094779.us-east-1.elb.amazonaws.com:3500/graphql';
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'http://internal-prod-Back-end-balancer-463094779.us-east-1.elb.amazonaws.com:3500/graphql';
