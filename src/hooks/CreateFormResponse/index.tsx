20f4bb6119f753d6c9e8d7ad9ca2deaac3a0965f
Juan David Cabrera Duran <juancabdu@unisabana.edu.co>
2021-04-07 20:25:12 -0500

    deleted auth from the hooks

--- src/hooks/CreateFormResponse/index.tsx
+++ src/hooks/CreateFormResponse/index.tsx
@@ -6,7 +6,6 @@ import {
   UPDATE_ANSWERED_QUESTIONS,
 } from '../../Context/FormQuestions/ActionTypes';
 import {ICreateAnswerResponse} from '../../Config/api/Graphql/Types';
-import {Auth} from 'aws-amplify';
 
 export interface ISaveResponseParams {
     variables:{
