4cb2f3fd8041b9e5110969785898f767f2f93e0b
SocksHead <juancapoar@gmail.com>
2021-04-23 18:47:59 -0500

    json updated

--- src/Context/FormQuestions/branchQuestions.ts
+++ src/Context/FormQuestions/branchQuestions.ts
@@ -10,7 +10,55 @@ export interface IQoption{
 
 export const BranchingLogic: IBranchingLogic[] = [
   {
+    id: "L2",
+    logicQuestions: [
+      { id: "10", answer: "US" },
+    ],
+  },
+  {
+    id: "L3",
+    logicQuestions: [
+      { id: "10", answer: "Some Other Country" },
+    ],
+  },
+  {
     id: '13',
     logicQuestions: [{id: '12', answer: 'Yes'}]
+  },
+  {
+    id: '18',
+    logicQuestions: [{id: '17', answer: 'Never Married'}]
+  },
+  {
+    id: '18',
+    logicQuestions: [{id: '12', answer: 'Divorced'}]
+  },
+  {
+    id: '18',
+    logicQuestions: [{id: '12', answer: 'Separated'}]
+  },
+  {
+    id: '18',
+    logicQuestions: [{id: '12', answer: 'Widowed'}]
+  },
+  {
+    id: '20',
+    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
+  },
+  {
+    id: '21',
+    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
+  },
+  {
+    id: '22',
+    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
+  },
+  {
+    id: '23',
+    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
+  },
+  {
+    id: '23',
+    logicQuestions: [{id: '20', answer: 'Yes'}]
   },
 ];
