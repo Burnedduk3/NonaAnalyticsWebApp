0685f1bc951aa5eaa1df6534ac3e0dd2e3388df0
SocksHead <juancapoar@gmail.com>
2021-04-27 16:37:04 -0500

    json with braching logic ended

--- src/Context/FormQuestions/branchQuestions.ts
+++ src/Context/FormQuestions/branchQuestions.ts
@@ -1,64 +1,281 @@
 export interface IBranchingLogic {
-  id: string
-  logicQuestions: IQoption[]
+    id: string;
+    logicQuestions: IQoption[];
 }
 
-export interface IQoption{
-  id: string
-  answer: string
+export interface IQoption {
+    id: string;
+    answer: string;
 }
 
 export const BranchingLogic: IBranchingLogic[] = [
-  {
-    id: "L2",
-    logicQuestions: [
-      { id: "10", answer: "US" },
-    ],
-  },
-  {
-    id: "L3",
-    logicQuestions: [
-      { id: "10", answer: "Some Other Country" },
-    ],
-  },
-  {
-    id: '13',
-    logicQuestions: [{id: '12', answer: 'Yes'}]
-  },
-  {
-    id: '18',
-    logicQuestions: [{id: '17', answer: 'Never Married'}]
-  },
-  {
-    id: '18',
-    logicQuestions: [{id: '12', answer: 'Divorced'}]
-  },
-  {
-    id: '18',
-    logicQuestions: [{id: '12', answer: 'Separated'}]
-  },
-  {
-    id: '18',
-    logicQuestions: [{id: '12', answer: 'Widowed'}]
-  },
-  {
-    id: '20',
-    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
-  },
-  {
-    id: '21',
-    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
-  },
-  {
-    id: '22',
-    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
-  },
-  {
-    id: '23',
-    logicQuestions: [{id: '19', answer: 'FEMALE & >0'}]
-  },
-  {
-    id: '23',
-    logicQuestions: [{id: '20', answer: 'Yes'}]
-  },
+    {
+        id: '272',
+        logicQuestions: [{ id: '10', answer: 'US' }],
+    },
+    {
+        id: '273',
+        logicQuestions: [{ id: '10', answer: 'Some Other Country' }],
+    },
+    {
+        id: '13',
+        logicQuestions: [{ id: '12', answer: 'Yes' }],
+    },
+    {
+        id: '18',
+        logicQuestions: [
+            { id: '17', answer: 'Never Married' },
+            { id: '17', answer: 'Divorced' },
+            { id: '17', answer: 'Separated' },
+            { id: '17', answer: 'Widowed' },
+        ],
+    },
+    {
+        id: '20',
+        logicQuestions: [
+            { id: '7', answer: 'Female' },
+            { id: '19', answer: '0' },
+        ],
+    },
+    {
+        id: '21',
+        logicQuestions: [
+            { id: '7', answer: 'Female' },
+            { id: '19', answer: '0' },
+        ],
+    },
+    {
+        id: '22',
+        logicQuestions: [
+            { id: '7', answer: 'Female' },
+            { id: '19', answer: '0' },
+        ],
+    },
+    {
+        id: '23',
+        logicQuestions: [
+            { id: '7', answer: 'Female' },
+            { id: '19', answer: '0' },
+            { id: '20', answer: 'Yes' },
+        ],
+    },
+    {
+        id: '25',
+        logicQuestions: [
+            { id: '19', answer: '0' },
+            { id: '24', answer: '0' },
+        ],
+    },
+    {
+        id: '56',
+        logicQuestions: [{ id: '19', answer: '0' }],
+    },
+    {
+        id: '57',
+        logicQuestions: [{ id: '19', answer: '0' }],
+    },
+    {
+        id: '59',
+        logicQuestions: [{ id: '6', answer: '50' }],
+    },
+    {
+        id: '60',
+        logicQuestions: [{ id: '6', answer: '50' }],
+    },
+    {
+        id: '61',
+        logicQuestions: [{ id: '6', answer: '50' }],
+    },
+    {
+        id: '62',
+        logicQuestions: [{ id: '6', answer: '50' }],
+    },
+    {
+        id: '63',
+        logicQuestions: [{ id: '6', answer: '50' }],
+    },
+    {
+        id: '64',
+        logicQuestions: [
+            { id: '6', answer: '50' },
+            { id: '7', answer: 'Male' },
+        ],
+    },
+    {
+        id: '65',
+        logicQuestions: [
+            { id: '6', answer: '40' },
+            { id: '7', answer: 'Female' },
+        ],
+    },
+    {
+        id: '66',
+        logicQuestions: [{ id: '7', answer: 'Female' }],
+    },
+    {
+        id: '67',
+        logicQuestions: [{ id: '7', answer: 'Female' }],
+    },
+    {
+        id: '77',
+        logicQuestions: [{ id: '7', answer: 'Female' }],
+    },
+    {
+        id: '88',
+        logicQuestions: [{ id: '85', answer: '0' }],
+    },
+    {
+        id: '90',
+        logicQuestions: [{ id: '86', answer: '0' }],
+    },
+    {
+        id: '91',
+        logicQuestions: [{ id: '87', answer: '0' }],
+    },
+    {
+        id: '95',
+        logicQuestions: [{ id: '94', answer: 'Yes' }],
+    },
+    {
+        id: '96',
+        logicQuestions: [{ id: '94', answer: 'Yes' }],
+    },
+    {
+        id: '97',
+        logicQuestions: [{ id: '94', answer: 'Yes' }],
+    },
+    {
+        id: '99',
+        logicQuestions: [{ id: '98', answer: 'Yes' }],
+    },
+    {
+        id: '100',
+        logicQuestions: [{ id: '98', answer: 'Yes' }],
+    },
+    {
+        id: '101',
+        logicQuestions: [{ id: '98', answer: 'Yes' }],
+    },
+    {
+        id: '111',
+        logicQuestions: [{ id: '110', answer: 'Yes' }],
+    },
+    {
+        id: '116',
+        logicQuestions: [{ id: '115', answer: '0' }],
+    },
+    {
+        id: '131',
+        logicQuestions: [{ id: '130', answer: '0' }],
+    },
+    {
+        id: '132',
+        logicQuestions: [{ id: '130', answer: '0' }],
+    },
+    {
+        id: '133',
+        logicQuestions: [{ id: '130', answer: '0' }],
+    },
+    {
+        id: '134',
+        logicQuestions: [{ id: '130', answer: '0' }],
+    },
+    {
+        id: '190',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '191',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '192',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '193',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '194',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '195',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '196',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '197',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '198',
+        logicQuestions: [
+            { id: '26', answer: 'Full-time employment' },
+            { id: '26', answer: 'Part-time employment' },
+            { id: '26', answer: 'Self-employed' },
+            { id: '26', answer: 'On maternity leave' },
+            { id: '26', answer: 'On illness leave' },
+        ],
+    },
+    {
+        id: '200',
+        logicQuestions: [{ id: '199', answer: '0' }],
+    },
+    {
+        id: '202',
+        logicQuestions: [{ id: '201', answer: '0' }],
+    },
 ];
